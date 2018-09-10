2bb311b17bbc6e5ec75c5024629108e9ed79f318
Troy Crawford <tacrawford91@gmail.com>
2018-09-09 20:52:36 -0500

    Added index page styling

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
 import {Link} from 'react-router-dom';
+import {Timeline} from 'react-twitter-widgets';
 
 import _ from 'lodash';
 
@@ -12,25 +13,55 @@ class PostsIndex extends Component {
     renderPosts() {
         return _.map(this.props.posts, post => {
             return (
-                <li className='list-group-item' key={post.id}>
-                    <Link to={`/posts/${post.id}`} >{post.day} - {post.summary} </Link>
-                </li>
-            );
+                <div className="card customCard" key={post.id} >
+                    <img className="card-img-top" src={post.imageURL} alt="Card image cap" />
+                    <div className="card-body">
+                        <h3 className="card-title">{post.day}</h3>
+                        <h5 className="card-title">{post.language}</h5>
+                        <h5 className="card-title">{post.skill}</h5>
+                        <p className="card-text">{post.summary}</p>
+                        <Link to={`/posts/${post.id}`} className="btn btn-primary">Read More</Link>
+                    </div>
+                </div>
+            )
         });
     };
 
     render() {
         return (
             <div>
-                <div className='text-xs-right'>
-                    <Link className='btn btn-primary' to='/posts/new'>
-                        Add a Post
-                    </Link>
+                <div class="jumbotron" id='customJumbo'>
+                    <div class="container">
+                        <h1 class="display-8">Troy A. Crawford</h1>
+                        <p class="lead">Welcome! I built this learning log as an extension of the basic log that can be found <a href='https://tac-basic-learninglog.herokuapp.com/'>here!</a> This log utilizes react and redux for the application view and state management. The application hits the original log to pull in content (custom api). The log is a centralized location to hold all of my new found knowledge. Below I break down the day's main take away. Please feel free to reach out on twitter to ask questions or leave comments. When WE all learn, WE all win!</p>
+                        <a href="https://troycrawford.tech" class="text-center"><h4>Portfolio: https://troycrawford.tech</h4></a>
+                        <hr class="jumbohr"></hr>
+                    </div>
                 </div>
-                <h3>Posts</h3>
-                <ul className="list-group">
-                    {this.renderPosts()}
-                </ul>
+                <div className='container'>                
+                    <div className="row">
+                        <div className='col-md-8'>
+                            <div className='postDiv'>
+                                {this.renderPosts()}
+                            </div>
+                        </div>
+                        <div className='col-md-4'>
+                            <div className='twitterDiv'>
+                                <Timeline 
+                                dataSource={{
+                                    sourceType: 'profile',
+                                    screenName: 'deveLOVEper1234'
+                                }}
+                                ></Timeline>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='text-xs-right'>
+                        <Link className='btn btn-primary hideBtn' to='/posts/new'>
+                            Add a Post
+                        </Link>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -25,4 +25,4 @@ ReactDOM.render(
       </Switch>
     </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.app'));
