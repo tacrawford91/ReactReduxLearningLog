adf6a7d64a0fe9c0ce4fe4bc5cb84544df022df5
Troy Crawford <tacrawford91@gmail.com>
2018-09-12 22:07:36 -0500

    redirect + public

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.listen(PORT, function() {
 });
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 })
